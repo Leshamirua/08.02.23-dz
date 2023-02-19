e022ceaf871b0e343ba6dded8bf3c8d4e10e147b
Leshamir <anastasy34@gmail.com>
2023-02-19 15:52:07 +0200

    Добавьте файлы проекта.

---
+++ 08.02.23  dz/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _08._02._23__dz
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Vector a= new Vector();
+            Vector b= new Vector(1);
+            Vector c= new Vector(1,2,4);
+            c.PrintVector();
+            Console.WriteLine();
+            a.InputVector();
+            Console.WriteLine();
+            Console.WriteLine(c.VectorLength());
+            Console.WriteLine();
+            c.Increase(12);
+            c.PrintVector();
+            c.Decrease(3);
+            c.PrintVector();
+            Console.WriteLine();
+            c = a + b;
+            c.PrintVector();
+            Console.WriteLine();
+            a = b * c;
+            a.PrintVector();
+            Console.WriteLine();
+            Console.WriteLine();
+            Console.WriteLine();
+            a.PrintVector();
+            Console.WriteLine();
+            c.PrintVector();
+            Console.WriteLine(c != a);
+            Console.WriteLine("\n\n\tBYE!\n\n");
+        }
+    }
+}
---
+++ 08.02.23  dz/Vector.cs
@@ -0,0 +1,92 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.AccessControl;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _08._02._23__dz
+{
+    internal class Vector
+    {
+        public int coordinateX { get; set; }
+        public int coordinateY { get; set; }
+        public int coordinateZ { get; set; }
+        public Vector()
+        {
+
+        }
+        public Vector(int coordinateX) : this(coordinateX, 0, 0)
+        {
+        
+        }
+        public Vector(int coordinateX, int coordinateY, int coordinateZ)
+        {
+            this.coordinateX = coordinateX;
+            this.coordinateY = coordinateY;
+            this.coordinateZ = coordinateZ;
+        }
+        public void PrintVector()
+        {
+            Console.WriteLine($"X: {coordinateX}\nY: {coordinateY}\nZ: {coordinateZ}");
+        }
+        public void InputVector()
+        {
+            Console.WriteLine($"X: {coordinateX = Convert.ToInt32(Console.ReadLine())}");
+            Console.WriteLine($"Y: {coordinateY = Convert.ToInt32(Console.ReadLine())}");
+            Console.WriteLine($"Z: {coordinateZ = Convert.ToInt32(Console.ReadLine())}");
+        }
+        public double VectorLength()
+        {
+            return Math.Abs(Math.Sqrt(Math.Pow(coordinateX, 2)+Math.Pow(coordinateY,2)+Math.Pow(coordinateZ,2)));
+        }
+        public void Increase(int value)
+        {
+            coordinateX += value;
+            coordinateY += value;
+            coordinateZ += value;
+        }
+        public void Decrease(int value)
+        {
+            coordinateX -= value;
+            coordinateY -= value;
+            coordinateZ -= value;
+        }
+        static public Vector operator+(Vector a, Vector b)
+        {
+            Vector v = new Vector();
+            v.coordinateX = a.coordinateX+b.coordinateX;
+            v.coordinateY = a.coordinateY+b.coordinateY;
+            v.coordinateZ = a.coordinateZ+b.coordinateZ;
+            return v;
+        }
+        static public Vector operator -(Vector a, Vector b)
+        {
+            Vector v = new Vector();
+            v.coordinateX = a.coordinateX - b.coordinateX;
+            v.coordinateY = a.coordinateY - b.coordinateY;
+            v.coordinateZ = a.coordinateZ - b.coordinateZ;
+            return v;
+        }
+        static public Vector operator *(Vector a, Vector b)
+        {
+            Vector v = new Vector();
+            v.coordinateX = a.coordinateX*b.coordinateX;
+            v.coordinateY = a.coordinateY*b.coordinateY;
+            v.coordinateZ = a.coordinateY * b.coordinateY;
+            return v;
+        }
+        static public bool operator ==(Vector a, Vector b)
+        {
+            if (a.coordinateX != b.coordinateX || a.coordinateY != b.coordinateY || a.coordinateZ != b.coordinateZ) 
+                    { return false; }
+            return true;
+        }
+        static public bool operator !=(Vector a, Vector b)
+        {
+            if (a.coordinateX == b.coordinateX || a.coordinateY == b.coordinateY || a.coordinateZ == b.coordinateZ) 
+            { return false; }
+                return true;
+        }
+    }
+}
